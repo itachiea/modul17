4f7aefbb9d84e9b18511934ddb3c88f65ab7f26a
ichi <w.konrad60@gmail.com>
2017-12-02 11:53:55 +0100

    zadanie 17.4

--- server.js
+++ server.js
@@ -1,27 +1,22 @@
-var fs = require('fs');
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var datstring;
+app.use(express.static('assets'));
 
-app.use(bodyParser.json());
-
-app.get('/getnote', function(req, res) {
-    console.log('GET ok');
-    fs.readFile('./test.json', 'utf-8', function(err, data) {
-        if (err) throw err;
-        datstring = data;
-        res.send(data);
-    });
+app.get('/', function(req, res) {
+    res.sendFile('/index.html');
 });
 
-app.post('/updatenote/:note', function(req, res) {
-    console.log('POST ok');
-    datstring = req.params.note;
-        fs.writeFile('./test.json', datstring, function(err) {
-            if(err) throw err;
-            console.log('dane dodane do pliku "test.json"');
-        });
+app.get('/userform', function(req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.end(JSON.stringify(response));
 });
 
-app.listen(3000);
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
